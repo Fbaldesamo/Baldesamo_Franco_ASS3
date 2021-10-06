d2a741527b31a749335bc70357534371c41a3dd3
Fbaldesamo <baldesamofranco@gmail.com>
2021-10-06 10:59:49 +0800

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿namespace Baldesamo_Franco_ASS3
+{
+    internal class Class1
+    {
+        public string name;
+        public string address;
+        public long tel_Num;
+        public int quanty;
+
+        public static bool ClassValid(string a, int b)
+        {
+            //method 1
+            if (a.Length <= b)
+            {
+                return true;
+            }
+            else
+            {
+                System.Console.WriteLine("should not more than {0}", b);
+                return false;
+            }
+        }
+
+        public static bool ClassValidInt(long a)
+        {
+            if (a.ToString().Length <= 11)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace Baldesamo_Franco_ASS3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                //length validation
+                int cNameLimit = 2;
+                int cAddress = 3;
+                bool customValidation = false;
+
+                //get the userinfos
+                Class1 custom1 = new Class1();
+                FileStream fs = new FileStream("Customer.txt", FileMode.Create);
+                StreamWriter sw = new StreamWriter(fs);
+
+                while (customValidation == false)
+                {
+                    Console.Write("Customer name: ");
+                    custom1.name = Console.ReadLine();
+                    customValidation = Class1.ClassValid(custom1.name, cNameLimit);
+                }
+                //will save the finalized name
+                sw.WriteLine("FirstName: " + custom1.name);
+
+                //get the address
+                customValidation = false;
+                while (customValidation == false)
+                {
+                    Console.Write("Customer address: ");
+                    custom1.address = Console.ReadLine();
+                    customValidation = Class1.ClassValid(custom1.address, cAddress);
+                }
+                sw.WriteLine("Address: " + custom1.address);
+
+                customValidation = false;
+                while (customValidation == false)
+                {
+                    Console.Write("Telephone number:");
+                    custom1.tel_Num = Int64.Parse(Console.ReadLine());//not yet done
+                    customValidation = Class1.ClassValidInt(custom1.tel_Num);
+                }  
+                sw.Close();
+                fs.Close();
+
+
+
+            }
+            catch
+            {
+                Console.WriteLine("Invalid Input!");
+
+            }
+            
+            Console.ReadKey();
+
+        }
+    }
+}
